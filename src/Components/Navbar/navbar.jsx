c973814656eda38c79a67e0280da0f7cd415ef64
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-03-01 20:32:43 +0530

    inialized

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,19 +1,75 @@
 import React from "react";
 import "./navbar.css";
-import { MdOutlineTravelExplore } from "react-icons";
+import { MdTravelExplore } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const navbar = () => {
   return (
     <section className="navBarSection">
       <header className="header flex">
         <div className="logoDiv">
-          <a href="#" className="logo flex">
+          <a href="#logo-flex" className="logo flex">
             <h1>
-              <MdOutlineTravelExplore className="icon" />
+              <MdTravelExplore className="icon" />
               Travel.
             </h1>
           </a>
         </div>
+        <div className="navBar">
+          <ul className="navLists flex">
+            <li className="navItems">
+              <a href="#navLink" className="navLink">
+                Home
+              </a>
+            </li>
+
+            <li className="navItems">
+              <a href="#navLink" className="navLink">
+                Packages
+              </a>
+            </li>
+
+            <li className="navItems">
+              <a href="#navLink" className="navLink">
+                Shop
+              </a>
+            </li>
+
+            <li className="navItems">
+              <a href="#navLink" className="navLink">
+                About
+              </a>
+            </li>
+
+            <li className="navItems">
+              <a href="#navLink" className="navLink">
+                Pages
+              </a>
+            </li>
+
+            <li className="navItems">
+              <a href="#navLink" className="navLink">
+                News
+              </a>
+            </li>
+
+            <li className="navItems">
+              <a href="#navLink" className="navLink">
+                Contact
+              </a>
+            </li>
+
+            <button className="btn">
+              <a href="#btn">BOOK NOW</a>
+            </button>
+          </ul>
+
+          <div className="classNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
+
+        <div className="toggleNavbar"></div>
       </header>
     </section>
   );
